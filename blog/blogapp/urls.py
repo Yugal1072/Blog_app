1e667ac91b046031b41a1aa31526bf27f5169e93
Yugal Govind <yugalgovind1072@gmail.com>
2023-07-06 00:30:46 +0530

    Authentication is Done!

---
+++ blog/blogapp/urls.py
@@ -0,0 +1,13 @@
+
+from django.urls import path
+from blogapp.views import *
+
+urlpatterns = [
+    path('', base, name='base'),
+    path('homepage/', homepage, name='homepage'),
+    path('signup_page/', signup_page, name='signup_page'),
+    path('login_page/', login_page, name='login_page'),
+    path('logout_page/', logout_page, name='logout_page'),
+]
+
+
---
+++ blog/blogapp/views.py
@@ -0,0 +1,80 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+
+# Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
+
+
+@login_required(login_url='/login_page')
+def homepage(request):
+    return render(request, 'homepage.html')
+
+
+# AUTHENTICATIONS
+
+def signup_page(request):
+    
+    if request.method == "POST":
+        first_name = request.POST['fname']
+        last_name = request.POST['lname']
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        user = User.objects.filter(username = username)
+
+        if user.exists():
+            messages.error(request, 'Username already Exist!')
+            return redirect('/signup_page')
+        
+        user = User.objects.create(
+                email = email, 
+                username = username,
+                first_name = first_name,
+                last_name = last_name,
+            
+        )
+        user.set_password(password)
+        user.save()
+        print(f"{user.first_name} Your account has been created Successfully!")
+        messages.success(request, f"Hello {first_name}, Your account has been created Successfully!")
+        return redirect('/signup_page')
+
+    return render (request, 'signup_page.html')
+
+# LOGIN
+def login_page(request):
+    
+    if request.method == 'POST':
+        
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        if not User.objects.filter(username = username).exists():
+            messages.error(request, "Invalid Username!")
+            return redirect('/login_page')
+
+        user = authenticate(username = username, password = password)
+    
+        if user is None:
+            messages.error(request, "Incorrect Password!")
+            return redirect('/login_page')
+        else:
+            login(request, user)
+            return redirect('/homepage')
+            
+    return render (request, 'login_page.html')
+
+
+# LOGOUT
+def logout_page(request):
+
+    logout(request)
+    return redirect('/login_page')
+
